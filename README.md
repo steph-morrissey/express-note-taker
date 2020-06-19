6ecdf6895189acbdbbcdd6a08a190bdddabf4727
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-06-19 14:49:44 +0100

    Added function to get notes from db.json

---
+++ README.md
@@ -0,0 +1,5 @@
+# Express Note Taker
+
+## Description
+
+- An application that can be used to write, save and delete notes.
--- src/public/assets/js/server.js
+++ src/public/assets/js/server.js
@@ -1,20 +1,42 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const { promisify } = require("util");
 
 const PORT = 3000;
 
 const app = express();
 
+// Promisify fs files
+const readFileAsync = promisify(fs.readFile);
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Returns index.html file
 const serveIndexFile = (req, res) => {
   const filePath = path.join(__dirname, "../../index.html");
   res.sendFile(filePath);
 };
+
+// Returns notes.html file
 const serveNotesFile = (req, res) => {
   const filePath = path.join(__dirname, "../../notes.html");
   res.sendFile(filePath);
 };
-const getNotes = (req, res) => {};
+
+// Reads db.json file and returns saved notes
+const getNotes = async (req, res) => {
+  const filePath = path.join(__dirname, "../../db/db.json");
+  console.log(filePath);
+  const notesData = await readFileAsync(filePath, "utf8");
+  const notes = JSON.parse(notesData);
+  res.json(notes);
+};
+
+// Receives new note and saves to db.json
 const saveNotes = (req, res) => {};
+
 // Routes
 app.get("/", serveIndexFile);
 app.get("/notes", serveNotesFile);
